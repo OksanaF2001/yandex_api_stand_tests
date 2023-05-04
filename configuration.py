e7499c3089791b4105e66cdb46002856eba26ccd
Alexander Filatov <alexanderfilatov@MacBook-Pro-Alexander.local>
2023-05-04 16:44:33 +0300

    My first commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://2196d6ba-d6ef-4081-b957-68c34ca686df.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
